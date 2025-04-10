00c1c526732f47bd0d98be0737790ea9b6c50f92
Khalil Saboor <khalilsaboor1@gmail.com>
2025-04-10 18:36:43 -0400

    Add initial Spring Boot project setup
    This commit initializes a basic Spring Boot project structured for a simple blog application. It includes Gradle configuration, essential dependencies (Spring Boot Starters, H2, Lombok), Git configuration files, and an initial application entry point with a placeholder test.

---
+++ src/test/java/io/cloutengineer/simpleblog/SimpleBlogApplicationTests.java
@@ -0,0 +1,14 @@
+package io.cloutengineer.simpleblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //* document why this method is empty
+    }
+
+}
